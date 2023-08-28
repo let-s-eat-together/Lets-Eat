085fbe0213e27ad2f25c863bf9e3206d34b389c4
이현재 <current11@naver.com>
2023-08-28 12:03:48 +0900

    start project

---
+++ LetsEat/LetsEat/LetsEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsEatApp.swift
+//  LetsEat
+//
+//  Created by 이현재 on 2023/08/28.
+//
+
+import SwiftUI
+
+@main
+struct LetsEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
