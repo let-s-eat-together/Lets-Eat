0aa531986146a1870c7d21cd06531992997d9949
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2023-09-11 13:39:49 +0900

    refactor: fix error

--- LetsEat/LetsEat/MessageView.swift
+++ LetsEat/LetsEat/MessageView.swift
@@ -28,7 +28,7 @@ struct MessageView: View {
                     VStack {
 //                        Text("\(item.date)~m ago")
                         Text("~m ago")
-                        stingButton(title: "나도 콕 찔러보기")
+//                        stingButton(title: "나도 콕 찔러보기")
                     }
                     .padding()
                 }
