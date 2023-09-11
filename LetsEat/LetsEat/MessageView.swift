ea95ca9ab15fb6d2e4d8cb3dae52ec6a6ea419dd
이현재 <current11@naver.com>
2023-09-11 13:14:34 +0900

    refactor: fix Text in message

--- LetsEat/LetsEat/MessageView.swift
+++ LetsEat/LetsEat/MessageView.swift
@@ -17,20 +17,10 @@ struct MessageView: View {
             ForEach(dataManager.getMessageDummyData(), id: \.self) { item in
                 HStack {
                     VStack(alignment: .leading) {
-                        Text(item.tag.rawValue)
+                        Text("콕 찔러보기")
                             .font(.system(.caption))
-                        switch item.tag {
-                        case .sting:
-                            Text("\(item.name)님이 콕 찔렀습니다.")
-//                        case .extend:
-//                            Text("\(item.name)님과의 약속 기간이\n연장되었습니다.")
-//                        case .request:
-//                            Text("\(item.name)님이 친구 요청을\n보냈습니다.")
-                        case .accept:
-                            Text("\(item.name)님이 수락했습니다.")
-                        default:
-                            Text("default")
-                        }
+                        
+                        Text("\(item.senderName)님이 회원님을 콕 찔렀습니다!")
                     }
                     
                     Spacer()
@@ -38,13 +28,7 @@ struct MessageView: View {
                     VStack {
 //                        Text("\(item.date)~m ago")
                         Text("~m ago")
-                        HStack {
-                            if item.tag == .sting {
-                                collectButton()
-                                
-                                rejectButton()
-                            }
-                        }
+                        stingButton(title: "나도 콕 찔러보기")
                     }
                     .padding()
                 }
@@ -63,42 +47,6 @@ struct MessageView: View {
     }
 }
 
-struct collectButton: View {
-    var body: some View {
-        Button {
-            sendCorrect()
-        } label: {
-            Image(systemName: "checkmark.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 25, height: 25)
-                .foregroundColor(.green)
-        }
-    }
-    
-    func sendCorrect() {
-        
-    }
-}
-
-struct rejectButton: View {
-    var body: some View {
-        Button {
-            sendReject()
-        } label: {
-            Image(systemName: "multiply.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 25, height: 25)
-                .foregroundColor(.red)
-        }
-    }
-    
-    func sendReject() {
-        
-    }
-    
-}
 struct MessageView_Previews: PreviewProvider {
     static var previews: some View {
         MessageView()
