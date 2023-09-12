d3badfb6062ec1043e5925aea9c537b1890fb19f
ddeonseo <eunseo1370@gmail.com>
2023-09-12 21:08:28 +0900

    essential complete

--- LetsEat/LetsEat/SettingView.swift
+++ LetsEat/LetsEat/SettingView.swift
@@ -1,109 +1,71 @@
-//
-//  SettingView.swift
-//  LetsEat
-//
-//  Created by 이현재 on 2023/08/28.
-//
-
 import SwiftUI
 
 struct SettingView: View {
-    @State private var isAlarm: Bool = false
+    @AppStorage("appTheme") var appTheme: String = "light" // 테마 지정
+    
+    @State private var isAlarmOn: Bool = false
+    @State private var isDarkOn: Bool = false
+    @State private var nickname: String = "eunseo"
+    @State private var editedNickname: String = ""
+    @State private var isEditingNickname: Bool = false
     
     var body: some View {
         VStack(alignment: .leading) {
-            
-
-            
-            //                Text("Settings")
-            //                    .padding()
-            //                    .font(.largeTitle)
-            //                    .bold()
-            //                    .multilineTextAlignment(.leading)
-            
-            HStack {
-                Image("discord")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .clipShape(Circle())
-                    .padding()
-                
-                
-                //                Spacer()
-                
-                VStack(alignment: .leading) {
-                    Text("Hyunjae Lee")
-                        .font(.title2)
-//                        .bold()
-//                        Text("Student")
-//                            .font(.caption)
-//                            .foregroundColor(.gray)
-//                        Button {
-//
-//                        } label: {
-//                            Text("Edit Profile")
-//                                .font(.system(size: 13))
-//                                .padding()
-//                                .tint(.white)
-//                                .frame(width: 100, height: 40)
-//                                .background(.blue)
-//                                .cornerRadius(25)
-//                        }
-                }
-            }
             List {
                 Section {
-                    NavigationLink {
-                        Text("Personal information detail")
-                    } label: {
-                        Image(systemName: "person")
-                        Text("프로필 설정")
-                    }
-//                    NavigationLink {
-//                        Text("Change Password detail")
-//                    } label: {
-//                        Image(systemName: "lock")
-//                        Text("Change Password")
-//                    }
-                    NavigationLink {
-                        Text("Personal information detail")
-                    } label: {
-                        HStack {
-                            Image(systemName: "moon")
-                            Text("Theme")
-                            Spacer()
-                            Text("Light")
-                                .foregroundColor(.gray)
-                                .font(.callout)
+                    HStack {
+                        Image("discord")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                            .padding(.all, 15)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                        VStack(alignment: .leading) {
+                            if isEditingNickname {
+                                TextField("변경할 이름", text: $editedNickname)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    .frame(width: 160)
+                                    .onAppear {
+                                        editedNickname = nickname
+                                    }
+                            } else {
+                                Text(nickname)
+                                    .font(.headline)
+                            }
+                        }
+                        Spacer()
+                        Button(action: {
+                            isEditingNickname.toggle()
+                            if !isEditingNickname {
+                                nickname = editedNickname
+                            }
+                        }) {
+                            Text(isEditingNickname ? "완료" : "프로필 수정")
+                                .foregroundColor(.blue)
                         }
                     }
                 }
-                Section {
-////                    NavigationLink {
-////                        Text("Logout View")
-////                    } label: {
-////                        Label("Logout", systemImage: "door.left.hand.open")
-//                    }
-                }
                 
                 Section {
-                    Toggle(isOn: $isAlarm) {
+                    Toggle(isOn: $isAlarmOn) {
                         Text("푸시 알림")
                     }
-                    Toggle(isOn: $isAlarm) {
-                        Text("푸시 알림")
+                    Toggle(isOn: $isDarkOn) {
+                        Text("다크 모드")
                     }
-                
+                    .onChange(of: isDarkOn) { newValue in
+                        appTheme = newValue ? "dark" : "light"
+                    }
                 }
             }
         }
-        .navigationTitle("설정")
+        .preferredColorScheme(appTheme == "dark" ? .dark : .light)
     }
-}
-
-struct SettingView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingView()
+    
+    struct SettingView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingView()
+        }
     }
 }
