7c970c53b6c14e1857acd359da04659fa5bbb5ca
ddeonseo <eunseo1370@gmail.com>
2023-09-28 16:32:29 +0900

    fix userId

--- LetsEat/LetsEat/Setting/SettingView.swift
+++ LetsEat/LetsEat/Setting/SettingView.swift
@@ -1,7 +1,7 @@
 import SwiftUI
+import Alamofire
 
 struct SettingView: View {
-//    @EnvironmentObject var username: String
     @AppStorage("appTheme") var appTheme: String = "light" // 테마 지정
     
     @State private var isAlarmOn: Bool = false
@@ -9,6 +9,7 @@ struct SettingView: View {
     @State private var nickname: String = "eunseo"
     @State private var editedNickname: String = ""
     @State private var isEditingNickname: Bool = false
+    let userId = user.id
     
     var isNicknameValid: Bool {
         return editedNickname.count >= 1
@@ -58,21 +59,37 @@ struct SettingView: View {
                     Toggle(isOn: $isAlarmOn) {
                         Text("푸시 알림")
                     }
-                    Toggle(isOn: $isDarkOn) {
-                        Text("다크 모드")
-                    }
-                    .onChange(of: isDarkOn) { newValue in
-                        appTheme = newValue ? "dark" : "light"
-                    }
+//                    Toggle(isOn: $isDarkOn) {
+//                        Text("다크 모드")
+//                    }
+//                    .onChange(of: isDarkOn) { newValue in
+//                        appTheme = newValue ? "dark" : "light"
+//                    }
                 }
             }
         }
-        .preferredColorScheme(appTheme == "dark" ? .dark : .light)
+//        .preferredColorScheme(appTheme == "dark" ? .dark : .light)
     }
     
+    private func updateNickname() {
+        let apiURL = "http://34.22.94.135:8080/rename"
+        let headers : HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        
+        let parameters: Parameters = [
+            "user_name": editedNickname,
+            "user_id": userId// 추가해야함
+        ]
+        
+        AF.request(apiURL, method: .put, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+            .validate(statusCode: 200..<300)
+    }
     struct SettingView_Previews: PreviewProvider {
         static var previews: some View {
             SettingView()
         }
     }
 }
+
+
