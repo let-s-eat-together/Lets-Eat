f94ffccafe103f3282cce5c2abd1fde817dbeab8
bellaria19 <52782842+bellaria19@users.noreply.github.com>
2023-10-03 17:28:22 +0900

    feat: add function for User and add TokenManager

--- LetsEat/LetsEat/Manager/UserManager.swift
+++ LetsEat/LetsEat/Manager/UserManager.swift
@@ -1,21 +1,49 @@
 //
 //  UserManager.swift
 //  LetsEat
-//
-//  Created by 이현재 on 2023/09/29.
-//
 
 import Foundation
-import Alamofire
 
 class UserManager: ObservableObject {
+    private let USER_KEY = "USER_KEY"
     static let shared = UserManager()
     
-    var userInfo: User = User(id: -1, token: "", nickname: "")
+    func saveUser(_ user: User) {
+        if let data = try? JSONEncoder().encode(user) {
+            UserDefaults.standard.set(data, forKey: USER_KEY)
+        }
+    }
     
-    func setInfo(id: Int, token: String, nickname: String) {
+    func getUser() -> User? {
+        if let userData = UserDefaults.standard.data(forKey: USER_KEY),
+           let user = try? JSONDecoder().decode(User.self, from: userData) {
+            return user
+        }
+        return nil
+    }
+
+    var userInfo: User = User(id: -1, username: "", token: "")
+    
+    func setInfo(id: Int, token: String) {
         userInfo.id = id
         userInfo.token = token
-        userInfo.nickname = nickname
+    }
+    
+    func setName(nickname: String) {
+        userInfo.username = nickname
+    }    
+}
+
+class TokenManager {
+    static let shared = TokenManager()
+    
+    private var userToken: String?
+    
+    func saveToken(_ token: String) {
+        userToken = token
+    }
+    
+    func getToken() -> String? {
+        return userToken
     }
 }
